8f8feb494c264dd9320d44d945de7465d042d9b6
Ibraheem Uthman <iamlonelypoet@gmail.com>
2022-06-01 15:37:28 +0100

    Preloader added

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,6 +222,7 @@ $(window).load(function() {
         });
     }
 
+
     /* ==============================================
     Preloader
     =============================================== */
@@ -229,6 +230,6 @@ $(window).load(function() {
     // will first fade out the loading animation
     $("#loading-animation").fadeOut();
     // will fade out the whole DIV that covers the website.
-    $("#preloader").delay(600).fadeOut("slow");
+    $("#preloader").delay(500).fadeOut("slow");
 
 });
